3ff75d3e6083c48db2e5c4549dd7291f4c07e642
Yomaira Escano <yjescano@gmail.com>
2018-01-23 17:15:56 +0000

    automatically backed up by learn

--- db/migrate/20180123170807_create_dogs.rb
+++ db/migrate/20180123170807_create_dogs.rb
@@ -9,4 +9,11 @@ class CreateDogs < ActiveRecord::Migration
   def down
     drop_table :dogs
   end
+
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
 end
